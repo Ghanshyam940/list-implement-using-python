236285f61614ef1c4a57e3520db9991dc0881038
gyanu <gyanub331@gmail.com>
2026-07-30 08:09:24 +0545

    problem solved

--- problem3.py
+++ problem3.py
@@ -1,7 +1,14 @@
-n=int(input("enter a number "))
-i=1
-sum=0
-while(i<=n):
-    sum+=i
-    i+=1
-    print(sum)
+# n=int(input("enter a number "))
+# i=1
+# sum=0
+# while(i<=n):
+#     sum+=i
+#     i+=1
+#     print(sum)
+
+
+n = int(input("enter a number"))
+product=1
+for i in range(1,n+1):
+    product=product*i
+    print(f"the factorial of a {n} is {product}")
