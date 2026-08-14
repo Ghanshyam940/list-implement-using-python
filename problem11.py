dae575fd4b7fdecc9da128ee6e7823ade50de5dd
gyanu <gyanub331@gmail.com>
2026-08-14 21:24:43 +0545

    problem solved

---
+++ problem11.py
@@ -0,0 +1,10 @@
+# sum of n natural numbers using recursion
+
+
+def sum(n):
+    if(n==1):
+        return 1
+    else:
+        return n + sum(n-1)
+n=int(input("Enter a number: "))
+print(f"The sum of first {n} natural numbers is: {sum(n)}")
